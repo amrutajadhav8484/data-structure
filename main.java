d32d017c73154b299bd8a683ff5f9bf6f095ddeb
AMRUTA JADHAV <87967851+amrutajadhav8484@users.noreply.github.com>
2023-10-08 09:37:09 +0530

    Create main.java
    bubble sort

---
+++ main.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+public class main {
+    public static void main(String[] args) {
+    int[] array={4,7,8,3,2,1};
+    bubble(array);
+    System.out.println(Arrays.toString(array));
+    }
+    static void bubble(int[]array)
+    {
+        boolean swapped;
+        // run the steps n-1 times
+        for(int i=0;i<array.length;i++)
+        {
+            swapped=false;
+            // for each step,max element will come at  last respective index
+            for(int j=1;j< array.length-i;j++)
+            {
+                //swap if previous element is smaller
+                if(array[j]<array[j-1])
+                {
+                    int temp=array[j];
+                    array[j]=array[j-1];
+                    array[j-1]=temp;
+                    swapped=true;
+                }
+            }
+            if(!swapped)
+            {
+                break;
+            }
+        }
+    }
+}
